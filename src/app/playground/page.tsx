fb150ce12dfdaf75c514668fb30f436ee811fee0
chemitaro <iwasi_44@hotmail.com>
2023-12-18 14:32:39 +0900

    Add AlertDialog component to PlaygroundPage

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -6,7 +6,17 @@ import {
 } from "@/components/ui/accordion"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Button } from '@/components/ui/button'
-
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 
 export default function PlaygroundPage() {
@@ -45,6 +55,27 @@ export default function PlaygroundPage() {
             You can add components and dependencies to your app using the cli.
           </AlertDescription>
         </Alert>
+      </div>
+      <div
+        className="m-3"
+      >
+        <p>Alert Dialog</p>
+        <AlertDialog>
+          <AlertDialogTrigger>Open</AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </>
   )
